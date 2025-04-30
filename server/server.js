46aeb96a634bb6c98f35d8f8fc4eb7255e8fbbb6
Prachi Yadav <prachiiyadav2409@gmail.com>
2025-04-30 10:48:09 +0530

    trying to deploy

--- server/server.js
+++ server/server.js
@@ -38,10 +38,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://e-commerce-app-1-652v.onrender.com",
-    ],
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
